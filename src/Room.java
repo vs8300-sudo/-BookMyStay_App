4352a0d85b2d0eb38865ce7be9d60504118acedc
vs8300-sudo <vs8300@srmist.edu.in>
2026-03-16 09:41:17 +0530

    UC2: Add hardcoded room models and initialization

---
+++ src/Room.java
@@ -0,0 +1,17 @@
+public abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
